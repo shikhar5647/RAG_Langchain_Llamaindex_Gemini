c310bf1bcbf29f8390134ff5dddf5feecad32f90
Shikhar Dave <120463805+shikhar5647@users.noreply.github.com>
2024-10-25 16:04:47 +0530

    Chat with websites

---
+++ conversational_RAG.py
@@ -0,0 +1,126 @@
+# Required installations (in terminal or at the start of your script if using Jupyter/Colab):
+#!pip install streamlit langchain langchain-google-genai langchain-community beautifulsoup4 python-dotenv chromadb
+
+import streamlit as st
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain_community.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import HuggingFaceBgeEmbeddings
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+
+# Load environment variables
+load_dotenv()
+
+# Initialize HuggingFace embeddings
+model_name = "BAAI/bge-small-en"
+model_kwargs = {"device": "cpu"}
+encode_kwargs = {"normalize_embeddings": True}
+hf = HuggingFaceBgeEmbeddings(
+    model_name=model_name, model_kwargs=model_kwargs, encode_kwargs=encode_kwargs
+)
+
+def get_vectorstore_from_url(url):
+    # Get the text in document form
+    loader = WebBaseLoader(url)
+    document = loader.load()
+    
+    # Split the document into chunks
+    text_splitter = RecursiveCharacterTextSplitter()
+    document_chunks = text_splitter.split_documents(document)
+    
+    # Create a vectorstore from the chunks using HuggingFace embeddings
+    vector_store = Chroma.from_documents(document_chunks, hf)
+
+    return vector_store
+
+def get_context_retriever_chain(vector_store):
+    llm = ChatGoogleGenerativeAI(
+        model="gemini-1.5-pro",
+        temperature=0,
+        max_tokens=None,
+        timeout=None,
+        max_retries=2,
+    )
+    
+    retriever = vector_store.as_retriever()
+    
+    prompt = ChatPromptTemplate.from_messages([
+      MessagesPlaceholder(variable_name="chat_history"),
+      ("user", "{input}"),
+      ("user", "Given the above conversation, generate a search query to look up in order to get information relevant to the conversation")
+    ])
+    
+    retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
+    
+    return retriever_chain
+
+def get_conversational_rag_chain(retriever_chain): 
+    llm = ChatGoogleGenerativeAI(
+        model="gemini-1.5-pro",
+        temperature=0,
+        max_tokens=None,
+        timeout=None,
+        max_retries=2,
+    )
+    
+    prompt = ChatPromptTemplate.from_messages([
+      ("system", "Answer the user's questions based on the below context:\n\n{context}"),
+      MessagesPlaceholder(variable_name="chat_history"),
+      ("user", "{input}"),
+    ])
+    
+    stuff_documents_chain = create_stuff_documents_chain(llm, prompt)
+    
+    return create_retrieval_chain(retriever_chain, stuff_documents_chain)
+
+def get_response(user_input):
+    retriever_chain = get_context_retriever_chain(st.session_state.vector_store)
+    conversation_rag_chain = get_conversational_rag_chain(retriever_chain)
+    
+    response = conversation_rag_chain.invoke({
+        "chat_history": st.session_state.chat_history,
+        "input": user_input
+    })
+    
+    return response['answer']
+
+# App config
+st.set_page_config(page_title="Chat with websites", page_icon="🤖")
+st.title("Chat with websites : By Shikhar Dave")
+
+# Sidebar
+with st.sidebar:
+    st.header("Settings")
+    website_url = st.text_input("Website URL")
+
+if website_url is None or website_url == "":
+    st.info("Please enter a website URL")
+else:
+    # Session state
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = [
+            AIMessage(content="Hello, I am a bot. How can I help you?"),
+        ]
+    if "vector_store" not in st.session_state:
+        st.session_state.vector_store = get_vectorstore_from_url(website_url)    
+
+    # User input
+    user_query = st.chat_input("Type your message here...")
+    if user_query is not None and user_query != "":
+        response = get_response(user_query)
+        st.session_state.chat_history.append(HumanMessage(content=user_query))
+        st.session_state.chat_history.append(AIMessage(content=response))
+        
+    # Conversation
+    for message in st.session_state.chat_history:
+        if isinstance(message, AIMessage):
+            with st.chat_message("AI"):
+                st.write(message.content)
+        elif isinstance(message, HumanMessage):
+            with st.chat_message("Human"):
+                st.write(message.content)
